b40bd05b3f2778082bace469bf97f9dd64301ac9
Anas Sheikh <anasshaikh975@gmail.com>
2025-04-22 17:40:32 +0500

    test

--- main.py
+++ main.py
@@ -19,5 +19,10 @@ def get_time():
     return {"status": "success", "time": current_time }
 
 
+@app.get("/new")
+def new_endpoint():
+    return {"message": "This is a new endpoint developed for testing!"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9001, reload=True)
