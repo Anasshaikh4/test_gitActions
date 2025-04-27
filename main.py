c6a12f167db37f9092e91bedddd09fc3540f71f8
Anas Sheikh <anasshaikh975@gmail.com>
2025-04-27 17:37:14 +0500

    nssm_01

--- main.py
+++ main.py
@@ -34,5 +34,9 @@ def test_endpoint01():
 def wajahat_endpoint():
     return {"message": "*** This is a test endpoint for WAJAHAT ! ***", "time": datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")}
 
+@app.get("/ali")
+def wajahat_endpoint():
+    return {"message": "*** This is a test endpoint for ALi ! ***", "time": datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S")}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9001, reload=True)
